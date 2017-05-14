3c911a97c747c8995622bd1fa88b2daca667b33d
Patrick Keisler <pkeisler@gmail.com>
2017-05-14 12:52:13 -0400

    Added new code

---
+++ Backups_in_36_Hours.sql
@@ -0,0 +1,11 @@
+-- Number of databases not backed up within the past 36 hours
+
+select COUNT(*) 
+from master.dbo.sysdatabases sbd
+where name not in ('master','model','msdb','tempdb','distribution')
+and not exists (
+select * from msdb.dbo.backupset bs
+where sbd.name = bs.database_name
+and bs.type = 'D'
+--and bs.backup_finish_date >= DATEADD(hh,-36,getdate()))
+and bs.backup_finish_date >= DATEADD(hh,-24,getdate()))
